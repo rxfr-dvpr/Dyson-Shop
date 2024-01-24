3fab32d61955c8054b2ed8395ba8df19e6616512
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-01-24 21:23:43 +0500

    opinion section added

---
+++ src/components/Opinion.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="opinion__section">
+    <div class="container">
+        <div class="row">
+            <div class="opinion__descr">
+                <h2 class="opinion__descr-title all-title">{{ store.title }}</h2>
+                <p class="opinion__descr-txt all-txt">{{ store.txt }}</p>
+            </div>
+
+            <img :src="store.img" alt="" class="opinion-img">
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { opinionStore } from "@/stores/opinionStore.js";
+
+export default {
+    name: 'Opinion Section',
+    data() {
+        return {
+            store: opinionStore()
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.opinion__section {
+    width: 100%;
+    background: #F8F8F8;
+    padding-top: 85px;
+
+    .row {
+        justify-content: space-between;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .opinion__descr {
+        max-width: 800px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        row-gap: 25px;
+    }
+
+    .opinion-img {
+        max-width: 500px;
+        width: 100%;
+    }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <Header/>
+
+  <main class="main">
+    <Opinion/>
+  </main>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Opinion from '@/components/Opinion.vue'
 
 export default {
   name: 'Home Page',
   components: {
-    Header
+    Header,
+    Opinion
   }
 }
 
 </script>
 
 <style lang="scss" scoped>
+
+.main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
